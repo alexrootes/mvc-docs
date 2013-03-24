05ae4d0f049a116d11df006fd7f86c4cc7c3c3b4
Alex Rootes <alexrootes@hotmail.co.uk>
2013-03-24 12:51:02 +0000

    Lucene proof of concept

--- src/MvcDocs/Controllers/DocumentsController.cs
+++ src/MvcDocs/Controllers/DocumentsController.cs
@@ -7,89 +7,128 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Web.Mvc;
+using Lucene.Net.Analysis.Standard;
+using Lucene.Net.Index;
+using Lucene.Net.QueryParsers;
+using Lucene.Net.Search;
+using Lucene.Net.Store;
 using MarkdownSharp;
 using MvcDocs.Models.Documents;
+using Version = Lucene.Net.Util.Version;
 
 namespace MvcDocs.Controllers
 {
-	public class DocumentsController : Controller
-	{
-		[HttpGet]
-		public ActionResult Index(string product, string language, string version)
-		{
-			// show TOC
+    public class DocumentsController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index(string product, string language, string version)
+        {
+            // show TOC
             var repo = FileHelper.GetRepositoryPath(product, language, version);
 
-		    IEnumerable<string> files;
+            IEnumerable<string> files;
             using (var reader = new DocumentRepositoryReader(repo))
             {
                 files = reader.GetMarkDownFiles();
             }
 
-			return View(files);
-		}
+            return View(files);
+        }
 
-		[HttpGet]
-		public ActionResult View(string product, string language, string version, string url)
-		{
-			var repo = Server.MapPath("~/App_Docs/");
+        public ActionResult Search(string term)
+        {
+            var results = new List<dynamic>();
 
-			var file = url.Split("/"[0]).Last().Replace("-", " ");
-			var title = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(file);
+            using (var directory = FSDirectory.Open(new DirectoryInfo("C:\\Temp\\index")))
+            using (var indexReader = IndexReader.Open(directory, true))
+            using (var indexSearch = new IndexSearcher(indexReader))
+            {
+                var analyzer = new StandardAnalyzer(Version.LUCENE_30);
 
-			// TODO: does file exist on disk, if so return it ...
-			if (file.EndsWith(".png"))
-			{
-				return File(Path.Combine(repo, product + "/" + language + "/" + version + "/" + file), "image/png");
-			}
+                // multi-field example
+                //var fields = new[] {"Make", "Model"};
+                //var queryParser = new MultiFieldQueryParser(Version.LUCENE_30, fields, analyzer);
 
-			var path = Path.Combine(repo, product + "/" + language + "/" + version + "/" + url + ".md");
+                var queryParser = new QueryParser(Version.LUCENE_30, "Make", analyzer);
+                var query = queryParser.Parse("Ford");
 
-			if (System.IO.File.Exists(path) == false)
-				return HttpNotFound();
+                var resultDocs = indexSearch.Search(query, indexReader.MaxDoc);
+                var hits = resultDocs.ScoreDocs;
+                foreach (var hit in hits)
+                {
+                    var doc = indexSearch.Doc(hit.Doc);
 
-			var raw = System.IO.File.ReadAllText(path, Encoding.UTF8);
+                    dynamic result = new ExpandoObject();
+                    result.Make = doc.Get("Make");
+                    result.Model = doc.Get("Model");
+                    results.Add(result);
+                }
+            }
 
-			var html = new Markdown(new MarkdownOptions()).Transform(raw);
+            return View(results);
+        }
 
-			const string internalLinkPattern = @"\[\[(.+?)\]\]"; // match: [[ any chars ]]
+        [HttpGet]
+        public ActionResult View(string product, string language, string version, string url)
+        {
+            var repo = Server.MapPath("~/App_Docs/");
 
-			Func<string, string> nameToDocPath = name =>
-			{
-				var encodedName = name.Replace(" ", "-");
+            var file = url.Split("/"[0]).Last().Replace("-", " ");
+            var title = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(file);
 
-				//return string.Join("/", folder, encodedName);
-				return encodedName.ToLower();
-			};
+            // TODO: does file exist on disk, if so return it ...
+            if (file.EndsWith(".png"))
+            {
+                return File(Path.Combine(repo, product + "/" + language + "/" + version + "/" + file), "image/png");
+            }
 
-			const string internalLinkTemplate = @"<a class='internal' href='{0}'>{1}</a>";
-			Func<Match, string> interalLinkResolver = match =>
-			{
-				// match.Value = [[text|doc_name]] or [[doc_name]]
-				var value = match.Value.Substring(2, match.Value.Length - 4);
+            var path = Path.Combine(repo, product + "/" + language + "/" + version + "/" + url + ".md");
 
-				var text = (value.Contains("|"))
-					? value.Substring(0, value.IndexOf("|", StringComparison.Ordinal))
-					: value;
+            if (System.IO.File.Exists(path) == false)
+                return HttpNotFound();
 
-				var name = (value.Contains("|"))
-					? value.Substring(value.IndexOf("|", StringComparison.Ordinal) + 1)
-					: value;
+            var raw = System.IO.File.ReadAllText(path, Encoding.UTF8);
 
-				var docPath = nameToDocPath(name);
-				var href = Url.Action("View", "Documents", new { product = "example", language = "en", version = "latest", url = docPath });
+            var html = new Markdown(new MarkdownOptions()).Transform(raw);
 
-				return string.Format(internalLinkTemplate, href, text);
-			};
+            const string internalLinkPattern = @"\[\[(.+?)\]\]"; // match: [[ any chars ]]
 
-			html = Regex.Replace(html, internalLinkPattern, match => interalLinkResolver(match));
+            Func<string, string> nameToDocPath = name =>
+            {
+                var encodedName = name.Replace(" ", "-");
 
-			dynamic doc = new ExpandoObject();
+                //return string.Join("/", folder, encodedName);
+                return encodedName.ToLower();
+            };
 
-			doc.Title = title;
-			doc.Content = html;
+            const string internalLinkTemplate = @"<a class='internal' href='{0}'>{1}</a>";
+            Func<Match, string> interalLinkResolver = match =>
+            {
+                // match.Value = [[text|doc_name]] or [[doc_name]]
+                var value = match.Value.Substring(2, match.Value.Length - 4);
 
-			return View(doc);
-		}
-	}
+                var text = (value.Contains("|"))
+                    ? value.Substring(0, value.IndexOf("|", StringComparison.Ordinal))
+                    : value;
+
+                var name = (value.Contains("|"))
+                    ? value.Substring(value.IndexOf("|", StringComparison.Ordinal) + 1)
+                    : value;
+
+                var docPath = nameToDocPath(name);
+                var href = Url.Action("View", "Documents", new { product = "example", language = "en", version = "latest", url = docPath });
+
+                return string.Format(internalLinkTemplate, href, text);
+            };
+
+            html = Regex.Replace(html, internalLinkPattern, match => interalLinkResolver(match));
+
+            dynamic doc = new ExpandoObject();
+
+            doc.Title = title;
+            doc.Content = html;
+
+            return View(doc);
+        }
+    }
 }
--- src/MvcDocs/Global.asax.cs
+++ src/MvcDocs/Global.asax.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Routing;
-using MvcDocs;
+using Lucene.Net.Analysis;
+using Lucene.Net.Analysis.Standard;
+using Lucene.Net.Documents;
+using Lucene.Net.Index;
+using Lucene.Net.Store;
+using Directory = Lucene.Net.Store.Directory;
+using Version = Lucene.Net.Util.Version;
 
 namespace MvcDocs
 {
@@ -19,6 +26,38 @@ namespace MvcDocs
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            BuildSearchIndex();
+        }
+
+        private static void BuildSearchIndex()
+        {
+            var fordFiesta = new Document();
+            fordFiesta.Add(new Field("Id", "1", Field.Store.YES, Field.Index.NOT_ANALYZED));
+            fordFiesta.Add(new Field("Make", "Ford", Field.Store.YES, Field.Index.ANALYZED));
+            fordFiesta.Add(new Field("Model", "Fiesta", Field.Store.YES, Field.Index.ANALYZED));
+
+            var fordFocus = new Document();
+            fordFocus.Add(new Field("Id", "2", Field.Store.YES, Field.Index.NOT_ANALYZED));
+            fordFocus.Add(new Field("Make", "Ford", Field.Store.YES, Field.Index.ANALYZED));
+            fordFocus.Add(new Field("Model", "Focus", Field.Store.YES, Field.Index.ANALYZED));
+
+            var vauxhallAstra = new Document();
+            vauxhallAstra.Add(new Field("Id", "3", Field.Store.YES, Field.Index.NOT_ANALYZED));
+            vauxhallAstra.Add(new Field("Make", "Vauxhall", Field.Store.YES, Field.Index.ANALYZED));
+            vauxhallAstra.Add(new Field("Model", "Astra", Field.Store.YES, Field.Index.ANALYZED));
+            
+            Directory directory = FSDirectory.Open(new DirectoryInfo("C:\\Temp\\index"));
+            Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_30);
+            
+            using (var writer = new IndexWriter(directory, analyzer, true, IndexWriter.MaxFieldLength.LIMITED))
+            {
+                writer.AddDocument(fordFiesta);
+                writer.AddDocument(fordFocus);
+                writer.AddDocument(vauxhallAstra);
+
+                writer.Optimize();
+            }
         }
     }
 }
